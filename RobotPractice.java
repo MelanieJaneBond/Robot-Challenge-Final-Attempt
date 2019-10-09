de8c5cac75395f9fc097bce521a85b755ad743a6
MelanieJaneBond <miss.bond2@gmail.com>
2019-10-09 09:09:01 -0500

    this might be my last commit

--- RobotPractice.java
+++ RobotPractice.java
@@ -1,4 +1,3 @@
-
 // Java program to demonstrate working of Robot 
 // class. This program is for Windoes. It opens 
 // a browser to the HPA url provided
@@ -7,19 +6,12 @@ import java.awt.AWTException;
 import java.awt.Robot; 
 import java.awt.event.KeyEvent; 
 import java.awt.event.InputEvent;
-import java.io.*;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException; 
 
-        // delay to allow the browser to fully load
-        //Then, the robot will begin to press keys and map over the elements of the page
-        //first, the robot will focus on the first box on the screen, then, it will press the mouse and release.
-        //After each box is complete, an alert needs time to load, so, add a delay long enough for that box to load.
-        //This program stops at the first attempt of box 4 because it randomly generates a required output and because
-        // I have yet to figure out how to access box 5 with just a keyboard.
-        //More research would be fun but I've taken quite a few days with this project. Can't wait to learn MORE!!
+
 
 public class RobotPractice 
 { 
@@ -47,13 +39,17 @@ public class RobotPractice
         // Create an instance of Robot class 
         Robot robot = new Robot(); 
   
-        robot.delay(2000);
+        robot.delay(1000);
+        // delay to allow the browser to fully load
+        //Then, the robot will begin to press keys and map over the elements of the page
+        //first, the robot will focus on the first box on the screen, then, it will press the mouse and release.
         robot.keyPress(KeyEvent.VK_TAB); 
-        Thread.sleep(500); 
+        Thread.sleep(1000); 
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         Thread.sleep(1000);
         robot.keyPress(KeyEvent.VK_ENTER);
+        //After each box is complete, an alert needs time to load, so, add a delay long enough for that box to load.
         Thread.sleep(1000);
         robot.keyPress(KeyEvent.VK_TAB);
         Thread.sleep(500);
@@ -65,7 +61,11 @@ public class RobotPractice
         Thread.sleep(1000);
         robot.keyPress(KeyEvent.VK_ENTER);
         Thread.sleep(500);
-        robot.keyPress(KeyEvent.VK_DOWN);
-        robot.keyPress(KeyEvent.VK_DOWN);
+        // robot.keyPress(KeyEvent.VK_DOWN);
+        // robot.keyPress(KeyEvent.VK_DOWN);
     } 
 } 
+//This program worked THROUGH step 3 when I first wrote it yesterday, but, some error that I cannot find must have appeared
+// it now stops after the first box.
+        // I have yet to figure out how to access box 5 with just a keyboard. A friend of mine who has worked in Java for years
+        // gave me a tip to check out "set x" but I'll research it later today and go ahead and turn in what I've done so far.
